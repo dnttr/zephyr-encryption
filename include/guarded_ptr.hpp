2464655c8c828d14d4c255f660269d9eee27046b
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-20 17:13:30 +0200

    Refactor guarded_ptr and memory classes for improved memory management

--- include/guarded_ptr.hpp
+++ include/guarded_ptr.hpp
@@ -7,10 +7,35 @@
 #include <memory>
 
 #include "data.hpp"
+#include "memory.hpp"
 
 namespace ze_kit
 {
-    class guarded_ptr : public std::unique_ptr<data, void *(*)(data *)>
+    class guarded_ptr : public std::unique_ptr<data, void (*)(data *)>
     {
+        using _deleter = void (*)(data *);
+
+        static void deleter(data *data)
+        {
+            if (!data)
+            {
+                return;
+            }
+
+            memory::deallocate(data->get_buffer(), data->get_size());
+            delete data;
+        }
+    public:
+        explicit guarded_ptr(data *ptr) : std::unique_ptr<data, _deleter>(ptr, deleter)
+        {
+        }
+
+        guarded_ptr() : guarded_ptr(nullptr)
+        {
+        }
+
+        explicit guarded_ptr(std::nullptr_t) : std::unique_ptr<data, _deleter>(nullptr, deleter)
+        {
+        }
     };
 }
--- include/memory.hpp
+++ include/memory.hpp
@@ -15,18 +15,15 @@ namespace ze_kit
     class memory
     {
     public:
+        [[nodiscard]] MALLOC
+        static unsigned char *allocate(size_t size);
+
+        static void deallocate(unsigned char *src , size_t size);
 
         [[nodiscard]] MALLOC
-        static void* allocate(size_t size);
-
-        static void deallocate(void* ptr);
-
-        [[nodiscard]] MALLOC
-        static void* copy(const void* src, size_t size);
-
-        static void zero(void* ptr, size_t size);
+        static unsigned char *copy(unsigned char *src, size_t size); //int8_t?
 
         [[nodiscard]]
-        static int compare(const void* ptr1, const void* ptr2, size_t size);
+        static bool compare(const void* ptr1, const void* ptr2, size_t size);
     };
 }
