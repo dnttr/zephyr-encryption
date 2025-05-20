a445792f6c608192819b27a6e701b422775d9bc9
dnttr <147146363+dnttr@users.noreply.github.com>
2025-05-20 17:42:22 +0200

    Rename lib.cpp to library.cpp and add memory management class with allocation, deallocation, copy, and comparison methods

---
+++ src/memory.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Damian Netter on 20/05/2025.
+//
+
+#include "memory.hpp"
+
+#include <cstring>
+#include <new>
+
+#include "sodium.h"
+
+namespace ze_kit
+{
+    unsigned char *memory::allocate(const size_t size)
+    {
+        const auto mem = sodium_malloc(size);
+
+        if (mem == nullptr)
+        {
+            throw std::bad_alloc();
+        }
+
+        return static_cast<unsigned char *>(mem);
+    }
+
+    void memory::deallocate(unsigned char *src, const size_t size)
+    {
+        if (src == nullptr)
+        {
+            return;
+        }
+
+        sodium_memzero(src, size);
+        sodium_free(src);
+
+        src = nullptr;
+    }
+
+    unsigned char *memory::copy(const unsigned char *src, const size_t size)
+    {
+        if (src == nullptr)
+        {
+            throw std::exception();
+        }
+
+        const auto mem = allocate(size);
+        memcpy(mem, src, size);
+
+        sodium_memzero(mem, size);
+
+        return mem;
+    }
+
+    bool memory::compare(const void *ptr1, const void *ptr2, const size_t size)
+    {
+        return sodium_memcmp(ptr1, ptr2, size) == 0;
+    }
+}
