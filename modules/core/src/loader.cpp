2f3cb4074d92cd432ec3c906e4618ecca0797e71
dnttr <147146363+dnttr@users.noreply.github.com>
2025-06-21 23:07:07 +0200

    Refactor loader methods for improved organization and clarity; use type aliases for better readability

--- modules/core/src/loader.cpp
+++ modules/core/src/loader.cpp
@@ -15,48 +15,56 @@
 #include "ZEKit/crypto_bridge/session_bridge.hpp"
 #include "ZEKit/crypto_bridge/signing_bridge.hpp"
 
-const std::unordered_multimap<std::string, znb_kit::jni_bridge_reference> ze_kit::loader::methods = {
-    {"ffi_ze_create_session", znb_kit::jni_bridge_reference(&session_bridge::create_session)},
-    {"ffi_ze_delete_session", znb_kit::jni_bridge_reference(&session_bridge::delete_session, {"long"})},
-    {"ffi_ze_close_library", znb_kit::jni_bridge_reference(&session_bridge::close_library)},
+using znb_kit::jni_bridge_reference;
 
-    {"ffi_ze_encrypt_data", znb_kit::jni_bridge_reference(&encryption_bridge::encrypt_data, {"long", "byte[]", "byte[]"})},
-    {"ffi_ze_decrypt_data", znb_kit::jni_bridge_reference(&encryption_bridge::decrypt_data, {"long", "byte[]", "byte[]"})},
-    {"ffi_ze_encrypt_with_public_key", znb_kit::jni_bridge_reference(&encryption_bridge::encrypt_with_public_key, {"long", "byte[]"})},
-    {"ffi_ze_decrypt_with_private_key", znb_kit::jni_bridge_reference(&encryption_bridge::decrypt_with_private_key, {"long", "byte[]"})},
+using znb_kit::LONG;
+using znb_kit::INT;
+using znb_kit::BYTE_ARRAY;
 
-    {"ffi_ze_generate_nonce", znb_kit::jni_bridge_reference(&data_bridge::generate_nonce, {"long", "int"})},
-    {"ffi_ze_get_nonce", znb_kit::jni_bridge_reference(&data_bridge::get_nonce, {"long", "int"})},
-    {"ffi_ze_set_nonce", znb_kit::jni_bridge_reference(&data_bridge::set_nonce, {"long", "int", "byte[]"})},
+const std::unordered_multimap<std::string, jni_bridge_reference> ze_kit::loader::methods = {
+    // Session Management
+    {"ffi_ze_create_session", jni_bridge_reference(&session_bridge::create_session)},
+    {"ffi_ze_delete_session", jni_bridge_reference(&session_bridge::delete_session, { LONG })},
+    {"ffi_ze_close_library", jni_bridge_reference(&session_bridge::close_library)},
 
-    {"ffi_ze_generate_keys", znb_kit::jni_bridge_reference(&data_bridge::generate_keys, {"long", "int"})},
-    {"ffi_ze_get_keypair", znb_kit::jni_bridge_reference(&data_bridge::get_keypair, {"long", "int"})},
+    // Key Exchange
+    {"ffi_ze_create_key_exchange", jni_bridge_reference(&exchange_bridge::create_key_exchange, { LONG })},
+    {"ffi_ze_process_key_exchange", jni_bridge_reference(&exchange_bridge::process_key_exchange, { LONG, BYTE_ARRAY })},
 
-    {"ffi_ze_set_partner_public_key", znb_kit::jni_bridge_reference(&data_bridge::set_partner_public_key, {"long", "byte[]"})},
+    // Encryption & Decryption
+    {"ffi_ze_encrypt_data", jni_bridge_reference(&encryption_bridge::encrypt_data, { LONG, BYTE_ARRAY, BYTE_ARRAY })},
+    {"ffi_ze_decrypt_data", jni_bridge_reference(&encryption_bridge::decrypt_data, { LONG, BYTE_ARRAY, BYTE_ARRAY })},
+    {"ffi_ze_encrypt_with_public_key", jni_bridge_reference(&encryption_bridge::encrypt_with_public_key, { LONG, BYTE_ARRAY })},
+    {"ffi_ze_decrypt_with_private_key", jni_bridge_reference(&encryption_bridge::decrypt_with_private_key, { LONG, BYTE_ARRAY })},
 
-    {"ffi_ze_create_signature", znb_kit::jni_bridge_reference(&signing_bridge::create_signature, {"long", "byte[]"})},
-    {"ffi_ze_verify_signature", znb_kit::jni_bridge_reference(&signing_bridge::verify_signature, {"long", "byte[]", "byte[]"})},
+    // Signing & Verification
+    {"ffi_ze_create_signature", jni_bridge_reference(&signing_bridge::create_signature, { LONG, BYTE_ARRAY })},
+    {"ffi_ze_verify_signature", jni_bridge_reference(&signing_bridge::verify_signature, { LONG, BYTE_ARRAY, BYTE_ARRAY })},
 
-    {"ffi_ze_generate_signing_keypair", znb_kit::jni_bridge_reference(&signing_bridge::generate_signing_keypair, {"long"})},
-    {"ffi_ze_derive_signing_keys", znb_kit::jni_bridge_reference(&signing_bridge::derive_signing_keys, {"long", "int"})},
-    {"ffi_ze_finalize_signing_key", znb_kit::jni_bridge_reference(&signing_bridge::finalize_signing_key, {"long", "int"})},
-    {"ffi_ze_set_signing_public_key", znb_kit::jni_bridge_reference(&signing_bridge::set_signing_public_key, {"long", "byte[]"})},
-    {"ffi_ze_get_base_signing_key", znb_kit::jni_bridge_reference(&signing_bridge::get_base_signing_key, {"long"})},
-
-    {"ffi_ze_create_key_exchange", znb_kit::jni_bridge_reference(&exchange_bridge::create_key_exchange, {"long"})},
-    {"ffi_ze_process_key_exchange", znb_kit::jni_bridge_reference(&exchange_bridge::process_key_exchange, {"long", "byte[]"})}
+    // Key & Nonce Management
+    {"ffi_ze_generate_keys", jni_bridge_reference(&data_bridge::generate_keys, { LONG, INT })},
+    {"ffi_ze_get_keypair", jni_bridge_reference(&data_bridge::get_keypair, { LONG, INT })},
+    {"ffi_ze_set_partner_public_key", jni_bridge_reference(&data_bridge::set_partner_public_key, { LONG, BYTE_ARRAY })},
+    {"ffi_ze_generate_signing_keypair", jni_bridge_reference(&signing_bridge::generate_signing_keypair, { LONG })},
+    {"ffi_ze_derive_signing_keys", jni_bridge_reference(&signing_bridge::derive_signing_keys, { LONG, INT })},
+    {"ffi_ze_finalize_signing_key", jni_bridge_reference(&signing_bridge::finalize_signing_key, { LONG, INT })},
+    {"ffi_ze_set_signing_public_key", jni_bridge_reference(&signing_bridge::set_signing_public_key, { LONG, BYTE_ARRAY })},
+    {"ffi_ze_get_base_signing_key", jni_bridge_reference(&signing_bridge::get_base_signing_key, { LONG })},
+    {"ffi_ze_generate_nonce", jni_bridge_reference(&data_bridge::generate_nonce, { LONG, INT })},
+    {"ffi_ze_get_nonce", jni_bridge_reference(&data_bridge::get_nonce, { LONG, INT })},
+    {"ffi_ze_set_nonce", jni_bridge_reference(&data_bridge::set_nonce, { LONG, INT, BYTE_ARRAY })},
 };
 
 const std::string ze_kit::loader::name = "org/dnttr/zephyr/network/bridge/internal/ZEKit";
 
-extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved) {
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     ze_kit::library::initialize();
 
     znb_kit::vm_management::jvmti_data jvmti_data;
     jvmti_data.version = JVMTI_VERSION_21;
     jvmti_data.capabilities.can_get_bytecodes = true;
 
-    ze_kit::vm_object = znb_kit::vm_management::wrap_vm(jvm, jvmti_data);
+    ze_kit::vm_object = znb_kit::vm_management::wrap_vm(vm, jvmti_data);
 
     znb_kit::jvmti_object jvmti(ze_kit::vm_object->get_env(), ze_kit::vm_object->get_jvmti()->get().get_owner());
     const znb_kit::klass_signature *klass = new znb_kit::klass_signature(ze_kit::vm_object->get_env(), ze_kit::loader::name);
